3705e80326ef4d48041b61220d7c99404d045a75
Loïc Mathieu <loikeseke@gmail.com>
2022-06-30 20:14:57 +0200

    WIP

--- deployment/src/main/java/io/quarkiverse/pinot/deployment/PinotProcessor.java
+++ deployment/src/main/java/io/quarkiverse/pinot/deployment/PinotProcessor.java
@@ -1,9 +1,13 @@
 package io.quarkiverse.pinot.deployment;
 
+import java.util.List;
+
 import io.quarkiverse.pinot.runtime.ConnectionProducer;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.RuntimeInitializedClassBuildItem;
 
 class PinotProcessor {
 
@@ -18,4 +22,18 @@ class PinotProcessor {
     AdditionalBeanBuildItem producer() {
         return new AdditionalBeanBuildItem(ConnectionProducer.class);
     }
+
+    @BuildStep
+    List<RuntimeInitializedClassBuildItem> runtimeInitializedClass() {
+        return List.of(
+                new RuntimeInitializedClassBuildItem("org.asynchttpclient.RequestBuilderBase"),
+                new RuntimeInitializedClassBuildItem("org.asynchttpclient.ntlm.NtlmEngine"),
+                new RuntimeInitializedClassBuildItem("org.apache.zookeeper.Login"),
+                new RuntimeInitializedClassBuildItem("org.apache.pinot.client.DynamicBrokerSelector"));
+    }
+
+    NativeImageResourceBuildItem nativeImageResource() {
+        return new NativeImageResourceBuildItem("**/ahc-default.properties");
+    }
+
 }
